39dcaba04c2fd6b66959e2e7acd19aeb6ea0aeb6
Adeilson Silva <adeilson@gmail.com>
2020-07-19 12:01:42 -0300

    - Incluido as images e sons - Aspect Ratio

--- lib/telas/Bichos.dart
+++ lib/telas/Bichos.dart
@@ -8,6 +8,40 @@ class Bichos extends StatefulWidget {
 class _BichosState extends State<Bichos> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    // var largura = MediaQuery.of(context).size.width;
+    // var altura = MediaQuery.of(context).size.height;
+    var aspectRatio = MediaQuery.of(context).size.aspectRatio * 2;
+
+    return GridView.count(
+      crossAxisCount: 2,
+      // scrollDirection: Axis.horizontal,
+      childAspectRatio: aspectRatio,
+      children: <Widget>[
+        GestureDetector(
+          onTap: () {},
+          child: Image.asset('assets/images/cao.png'),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: Image.asset('assets/images/gato.png'),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: Image.asset('assets/images/leao.png'),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: Image.asset('assets/images/macaco.png'),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: Image.asset('assets/images/ovelha.png'),
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: Image.asset('assets/images/vaca.png'),
+        ),
+      ],
+    );
   }
 }
